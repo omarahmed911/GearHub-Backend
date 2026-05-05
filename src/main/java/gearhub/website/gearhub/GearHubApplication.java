ac5ea77081f588698fbbf94dba8ad3681424e2a6
Omar Mahmoud <omarmahmoud911922@gmail.com>
2026-05-06 00:53:46 +0300

    creation commit

---
+++ src/main/java/gearhub/website/gearhub/GearHubApplication.java
@@ -0,0 +1,13 @@
+package gearhub.website.gearhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GearHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gearhub/website/gearhub/GearHubApplicationTests.java
@@ -0,0 +1,13 @@
+package gearhub.website.gearhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
